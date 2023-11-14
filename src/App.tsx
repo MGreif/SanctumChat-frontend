a202d2dca21b9d928c4990be9e967266d9559d8c
Mika Greif <mika@greif.me>
2023-11-14 01:48:32 +0100

    Add simple chat layout. Implement websocket connection to BE service

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import { Chat } from "./chat/Chat"
 import { MainPage } from "./MainPage"
+import { WebSocketProvider } from "react-use-websockets"
 
 export const App = () => {
     const routes = createBrowserRouter([{
--- src/chat/Chat.tsx
+++ src/chat/Chat.tsx
@@ -1,9 +1,37 @@
 import { useParams } from "react-router"
 import { Layout } from "../layout"
+import { useEffect, useRef, useState } from "react";
 
 export const Chat = () => {
     const params = useParams<{ chatId: string }>()
+    const connection = useRef<WebSocket | null>(null)
+    const [messages, setMessages] = useState<string[]>([])
+
+    useEffect(() => {
+        const socket = new WebSocket("ws://127.0.0.1:3000/ws")
+        // Connection opened
+        socket.addEventListener("open", (event) => {
+            socket.send("Connection established")
+        })
+
+        // Listen for messages
+        socket.addEventListener("message", (event) => {
+            setMessages(v => [v, event.data.toString()])
+            console.log("Message from server ", event.data)
+        })
+
+        connection.current = socket
+
+        return () => connection.current?.close()
+    }, [])
+
+    const handleClick = () => {
+        connection.current?.send("abc")
+    }
+
     return <Layout title="Chat">
         <span>{params.chatId}</span>
+        <button onClick={handleClick}>Send socket</button>
+        {messages.map((message, i) => <span key={message + i}>{message}</span>)}
     </Layout>
 }
